771fd3726fa7e589cd1059389ec9e4d4c4a918df
randomStudentES <jlirascand1@educacion.navarra.es>
2025-04-28 11:34:56 +0200

    generado

---
+++ Calculadora/src/test/java/org/example/CalculadoraTest.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTest {
+
+    @org.junit.jupiter.api.Test
+    void suma() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void resta() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiplicacion() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void division() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void esPar() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void maximo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void esDivisible() {
+    }
+}
