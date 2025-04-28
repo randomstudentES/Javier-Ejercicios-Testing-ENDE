b43cc6291994f086cb1f6b56a763651464bbaf6c
randomStudentES <jlirascand1@educacion.navarra.es>
2025-04-28 11:25:07 +0200

    empezado calculadora

--- Calculadora/src/test/java/org/example/CalculadoraTest.java
+++ Calculadora/src/test/java/org/example/CalculadoraTest.java
@@ -9,38 +9,35 @@ class CalculadoraTest {
     private Calculadora calculadora;
 
     @BeforeEach
-    public void setUp() {
+    void setUp(){
         calculadora = new Calculadora();
     }
 
     @org.junit.jupiter.api.Test
     void suma() {
-        assertEquals(5, calculadora.suma(2, 3));
     }
 
     @org.junit.jupiter.api.Test
     void resta() {
-        assertEquals(1, calculadora.resta(4, 3));
     }
 
     @org.junit.jupiter.api.Test
     void multiplicacion() {
-        assertEquals(12, calculadora.multiplicacion(4, 3));
     }
 
     @org.junit.jupiter.api.Test
     void division() {
-        assertEquals(2, calculadora.division(6, 3));
     }
 
     @org.junit.jupiter.api.Test
     void esPar() {
-        assertTrue(calculadora.esPar(4));
-        assertFalse(calculadora.esPar(3));
     }
 
     @org.junit.jupiter.api.Test
     void maximo() {
-        assertEquals(7, calculadora.maximo(5, 7, 2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void esDivisible() {
     }
 }
