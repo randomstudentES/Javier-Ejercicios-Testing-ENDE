3c7ed2f918ecf92e5b0de6ace2caf7d73d4f3c4c
randomStudentES <jlirascand1@educacion.navarra.es>
2025-04-28 11:42:13 +0200

    complejidad

--- Calculadora/src/test/java/org/example/CalculadoraTest.java
+++ Calculadora/src/test/java/org/example/CalculadoraTest.java
@@ -14,38 +14,38 @@ class CalculadoraTest {
     }
 
     @org.junit.jupiter.api.Test
-    void suma() {
+    void suma() { //complejidad 1
         assertEquals(3, calculadora.suma(2, 1));
     }
 
     @org.junit.jupiter.api.Test
-    void resta() {
+    void resta() { //complejidad 1
         assertEquals(1, calculadora.resta(2, 1));
     }
 
     @org.junit.jupiter.api.Test
-    void multiplicacion() {
+    void multiplicacion() { //complejidad 1
         assertEquals(6, calculadora.multiplicacion(3, 2));
     }
 
     @org.junit.jupiter.api.Test
-    void division() {
+    void division() { //complejidad 2
         assertEquals(2, calculadora.division(4, 2));
     }
 
     @org.junit.jupiter.api.Test
-    void esPar() {
+    void esPar() { //complejidad 2
         assertTrue(calculadora.esPar(4));
 
     }
 
     @org.junit.jupiter.api.Test
-    void maximo() {
+    void maximo() { //complejidad 3
         assertEquals(5, calculadora.maximo(1, 5, 4));
     }
 
     @org.junit.jupiter.api.Test
-    void esDivisible() {
+    void esDivisible() { //complejidad 3
         assertTrue(calculadora.esDivisible(6, 2));
     }
 }
